65ccab779a4c464d162b223fb66d6c29c0b8853b
yuvaneshkm <yuvaneshkm05@gmail.com>
2024-05-23 11:02:05 +0530

    'setup.cfg_file'

--- setup.py
+++ setup.py
@@ -7,15 +7,16 @@ warnings.filterwarnings('ignore')
 # ======================> SETUP <============================
 
 # basic info:
-PACKAGE_NAME = 'mongodbconnect'
+PACKAGE_NAME_ = 'mongodbconnect'
 __version__ = '0.0.1'
-AUTHOR_USER_NAME = 'yuvaneshkm'
-AUTHOR_EMAIL = 'yuvaneshkm05@gmail.com'
-REPO_NAME = 'mongodb-connector-pkg'
+AUTHOR_USER_NAME_ = 'yuvaneshkm'
+AUTHOR_EMAIL_ = 'yuvaneshkm05@gmail.com'
+REPO_NAME_ = 'mongodb-connector-pkg'
+LICENSE_ = 'MIT'
 
 # long description (reading the README.md file):
 with open('README.md', 'r', encoding='utf-8') as f:
-    LONG_DESCRIPTION = f.read()
+    LONG_DESCRIPTION_ = f.read()
 f.close()
 
 # requirements:
@@ -33,18 +34,19 @@ def get_requirements(requirements_file_path:str):
 
 # setup:
 setup(
-    name = PACKAGE_NAME,
+    name = PACKAGE_NAME_,
     version = __version__,
-    author = AUTHOR_USER_NAME,
-    author_email = AUTHOR_EMAIL,
+    author = AUTHOR_USER_NAME_,
+    author_email = AUTHOR_EMAIL_,
+    license = LICENSE_,
     description = 'Python Package for connecting MongoDB Database',
-    long_description =  LONG_DESCRIPTION,
+    long_description =  LONG_DESCRIPTION_,
     long_description_content_type = 'text/markdown',
-    url = f'https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}',
+    url = f'https://github.com/{AUTHOR_USER_NAME_}/{REPO_NAME_}',
     project_urls = {
-        'Bug Tracker':f'https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}/issues'
+        'Bug Tracker':f'https://github.com/{AUTHOR_USER_NAME_}/{REPO_NAME_}/issues'
     },
     package_dir = {'':'src'},
     packages = find_packages(where='src'),
-    install_requires = get_requirements('requirements.txt')
+    install_requires = get_requirements('requirements_dev.txt')
 )
