914fe84cb1b427c8e2168ed6d0110d8a40eff0d4
yuvaneshkm <yuvaneshkm05@gmail.com>
2024-05-29 21:20:36 +0530

    updated

--- setup.py
+++ setup.py
@@ -1,53 +1,29 @@
-# ====================> importing necessary packages <========================
 from setuptools import setup, find_packages
 from typing import List
-import warnings
-warnings.filterwarnings('ignore')
 
-
-# ======================> SETUP <============================
-
-# basic info:
-PACKAGE_NAME_ = 'mongodbconnect'
-__version__ = '0.0.1'
-AUTHOR_USER_NAME_ = 'yuvaneshkm'
-AUTHOR_EMAIL_ = 'yuvaneshkm05@gmail.com'
-REPO_NAME_ = 'mongodb-connector-pkg'
-LICENSE_ = 'MIT'
-
-# long description (reading the README.md file):
 with open('README.md', 'r', encoding='utf-8') as f:
-    LONG_DESCRIPTION_ = f.read()
-f.close()
+    long_description = f.read()  
+f.close()   
+   
 
-# requirements:
-def get_requirements(requirements_file_path:str) -> List[str]:
+__version__ = "0.0.1"
+REPO_NAME = "mongodb-connector-pkg"
+PKG_NAME= "databaseautomation"
+AUTHOR_USER_NAME = "yuvaneshkm"
+AUTHOR_EMAIL = "yuvaneshkm27@gmail.com"
 
-    with open(requirements_file_path, 'r') as f:
-        require = f.read()
-        requirements = require.split('\n')
-        # remove '-e .
-        if '-e .' in requirements:
-            requirements.remove('-e .')
-    f.close()
-
-    return requirements
-
-# setup:
 setup(
-    name = PACKAGE_NAME_,
-    version = __version__,
-    author = AUTHOR_USER_NAME_,
-    author_email = AUTHOR_EMAIL_,
-    license = LICENSE_,
-    description = 'Python Package for connecting MongoDB Database',
-    long_description =  LONG_DESCRIPTION_,
-    long_description_content_type = 'text/markdown',
-    url = f'https://github.com/{AUTHOR_USER_NAME_}/{REPO_NAME_}',
-    project_urls = {
-        'Bug Tracker':f'https://github.com/{AUTHOR_USER_NAME_}/{REPO_NAME_}/issues'
+    name=PKG_NAME,
+    version=__version__,
+    author=AUTHOR_USER_NAME,
+    author_email=AUTHOR_EMAIL,
+    description="A python package for connecting with database.",
+    long_description=long_description,
+    long_description_content="text/markdown",
+    url=f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}",
+    project_urls={
+        "Bug Tracker": f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}/issues",
     },
-    package_dir = {'':'src'},
-    packages = find_packages(where='src'),
-    install_requires = get_requirements('requirements_dev.txt')
-)
+    package_dir={"": "src"},
+    packages=find_packages(where="src"),
+    )
