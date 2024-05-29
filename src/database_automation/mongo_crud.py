ac34856677f36d708a4532f3d2f8c6b1b883511f
yuvaneshkm <yuvaneshkm05@gmail.com>
2024-05-29 21:55:34 +0530

    updated

--- src/database_automation/mongo_crud.py
+++ src/database_automation/mongo_crud.py
@@ -1,15 +1,15 @@
 # importing necessary libraries:
-from typing import Any
 from pathlib import Path
 import pandas as pd
 from pymongo.mongo_client import MongoClient
 import json
 
+
 # creatring a class for mongodb connection:
 class MongoDBConnection:
 
     # constructor:
-    def __init__(self, client_url:str, database_name:str, collection_name:str):
+    def __init__(self, client_url: str, database_name: str, collection_name: str):
         self.client_url = client_url
         self.database_name = database_name
         self.collection_name = collection_name
@@ -17,7 +17,7 @@ class MongoDBConnection:
     # mongo client:
     def create_mongo_client(self):
         self.client = MongoClient(self.client_url)
-    
+
     # database:
     def database_(self):
         self.db = self.client[self.database_name]
@@ -28,14 +28,14 @@ class MongoDBConnection:
         return coll
 
     # insert single record:
-    def single_insert(self, record:dict):
+    def single_insert(self, record: dict):
         coll = self.collection_()
         coll.insert_one(record)
 
     # insert bulk record:
-    def bulk_insert(self, file_path:str):
-        file_path = Path(file_path)
-        
+    def bulk_insert(self, file_path: str):
+        file_path = str(Path(file_path))
+
         if file_path.split('.')[-1]=='json':
             with open(file_path, 'r') as file:
                 json_file_data = json.load(file)
@@ -43,7 +43,7 @@ class MongoDBConnection:
             coll = self.collection_()
             coll.insert_many(json_file_data)
 
-        elif file_path.split('.')[-1]=='csv':
+        elif file_path.split('.')[-1] == 'csv':
             df = pd.read_csv(file_path)
             csv_file_data = df.to_dict(orient='records')
             coll = self.collection_()
