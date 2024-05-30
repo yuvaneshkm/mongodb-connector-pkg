d895a7ffd5b74755d1a208f0113a4fe3a369a24d
yuvaneshkm <yuvaneshkm05@gmail.com>
2024-05-30 17:51:24 +0530

    update

--- setup.py
+++ setup.py
@@ -17,6 +17,19 @@ AUTHOR_EMAIL = "yuvaneshkm27@gmail.com"
 with open('README.md', 'r', encoding='utf-8') as f:
     long_description = f.read()  
 f.close()
+
+# requirements
+HYPEN_E_DOT='-e .'
+
+def get_requiremet(file_path:str)->List[str]:
+    requirements = []
+    with open(file_path) as f:
+        requirements=f.readlines()
+        requirements=[req.replace("\n","")for req in requirements]
+        
+        if HYPEN_E_DOT in requirements:
+            requirements.remove(HYPEN_E_DOT)
+    return requirements
    
 # setup:
 setup(
@@ -32,5 +45,6 @@ setup(
         "Bug Tracker": f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}/issues",
     },
     package_dir={"": "src"},
-    packages=find_packages(where="src")
+    packages=find_packages(where="src"),
+    install_requires = get_requiremet("./requirements.txt")
     )
