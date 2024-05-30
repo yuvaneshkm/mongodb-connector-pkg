c0ad5e20ad0d00b66a463a6e229f6d4406137a4d
yuvaneshkm <yuvaneshkm05@gmail.com>
2024-05-30 18:40:11 +0530

    updated

--- setup.py
+++ setup.py
@@ -18,18 +18,6 @@ with open('README.md', 'r', encoding='utf-8') as f:
     long_description = f.read()  
 f.close()
 
-# requirements:
-def parse_requirements(filename: str):
-    with open(filename, 'r') as file:
-        requires = file.read()
-        requirements = requires.split('\n')
-        if '-e .' in requirements:
-            requirements.remove('-e .')
-        file.close()
-
-    return requirements
-
-
 # setup:
 setup(
     name=PKG_NAME,
@@ -44,7 +32,5 @@ setup(
         "Bug Tracker": f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}/issues",
     },
     package_dir={"": "src"},
-    packages=find_packages(where="src"),
-    install_requires = parse_requirements('requirements.txt'),
-    extras_require = {'dev': parse_requirements('requirements_dev.txt')}
+    packages=find_packages(where="src")
     )
