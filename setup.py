2c99261f66c8dd69e435452fd8cc32f3a32acaf4
yuvaneshkm <yuvaneshkm05@gmail.com>
2024-05-30 16:21:57 +0530

    update

--- setup.py
+++ setup.py
@@ -19,14 +19,15 @@ with open('README.md', 'r', encoding='utf-8') as f:
 f.close()
 
 # requirements:
+HYPEN_E_DOT = '-e .'
 def get_requirements(requirements_file_path: str) -> List[str]:
 
     with open(requirements_file_path, 'r') as f:
         require = f.read()
         requirements = require.split('\n')
         # remove -e .
-        if '-e .' in requirements:
-            requirements.remove('-e .')
+        if HYPEN_E_DOT in requirements:
+            requirements.remove(HYPEN_E_DOT)
     f.close()
 
     return requirements
@@ -46,5 +47,5 @@ setup(
     },
     package_dir={"": "src"},
     packages=find_packages(where="src"),
-    install_requires = get_requirements('requirements_dev.txt')
+    install_requires = get_requirements('./requirements_dev.txt')
     )
