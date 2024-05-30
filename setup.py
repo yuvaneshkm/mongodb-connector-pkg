21684bf5c434e08e6319f420092b55cdc5f4d5dd
yuvaneshkm <yuvaneshkm05@gmail.com>
2024-05-30 16:28:53 +0530

    update

--- setup.py
+++ setup.py
@@ -47,5 +47,5 @@ setup(
     },
     package_dir={"": "src"},
     packages=find_packages(where="src"),
-    install_requires = get_requirements('./requirements_dev.txt')
+    install_requires = get_requirements('requirements.txt')
     )
